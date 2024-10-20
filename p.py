84d771c3fd7f01201fd6ae2fdb6397c502daf15b
CPS <98105494+cyberphantomsyndicate@users.noreply.github.com>
2024-10-20 12:24:51 +0530

    Update p.py

--- p.py
+++ p.py
@@ -6,3 +6,10 @@ from apiclient.discovery import build
 def get_parser():
     parser = ArgumentParser()
     parser.add_arguments('--query')
+    parser.add_arguement('--n',type=int, defoult=10)
+    return parser 
+    
+if ___name___== '___main___':
+    parser = parser.parse_args()
+    
+    api_key = os.environ.get('
